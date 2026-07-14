163fbfe23c6e303df0fe4ca4f0f730b28fea6148
Samiullah Khan <samiullahpanni@gmail.com>
2026-07-14 15:35:46 +0500

    Add: depth-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr){
+            return 0;
+        }
+        int depth = 0;
+        queue <TreeNode*> q;
+        q.emplace(root);
+        while(!q.empty()){
+            int levelsize = q.size();
+            for(int i = 0;i<levelsize;i++){
+                TreeNode* u = q.front();
+                q.pop();
+                if(u->left)q.emplace(u->left);
+                if(u->right)q.emplace(u->right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
