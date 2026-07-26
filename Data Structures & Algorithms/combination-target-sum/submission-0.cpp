607398c13fc8b70f87fe0b3f601359eac1e65f54
Samiullah Khan <samiullahpanni@gmail.com>
2026-07-26 14:58:31 +0500

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   private:
+    void backtrack(int start, int remaining, vector<int> &combo,vector<vector<int>> &result,vector<int>& nums) {
+        if (remaining == 0) {
+            result.push_back(combo);
+            return;
+        }
+        for (int i = start; i < nums.size(); i++) {
+            if (nums[i] > remaining) {
+                break;
+            }
+            combo.push_back(nums[i]);
+            backtrack(i, remaining - nums[i], combo,result,nums);
+            combo.pop_back();
+        }
+    }
+
+   public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int>combo;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        backtrack(0, target, combo,result,nums);
+        return result;
+    }
+};
