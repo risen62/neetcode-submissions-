a8acda2b32072ad21d119f8de8664afc3c29c105
Samiullah Khan <samiullahpanni@gmail.com>
2026-08-13 15:24:47 +0500

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        
+        if (s[0] == '0') {
+            dp[1] = 0;
+        } else {
+            dp[1] = 1;
+        }
+        
+        for (int i = 2; i <= n; i++) {
+            int oneDigit = stoi(s.substr(i - 1, 1));
+            int twoDigits = stoi(s.substr(i - 2, 2));
+            
+            if (oneDigit >= 1) {
+                dp[i] += dp[i - 1];
+            }
+            
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        
+        return dp[n];
+    }
+};
