4c1e1e53fbc8dfa0a3e409fb3f5af56382bfc27e
Samiullah Khan <samiullahpanni@gmail.com>
2026-08-20 13:39:33 +0500

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> matrix(m,vector<int>(n,0));
+
+
+        for(int i = 0;i<n;i++){
+            matrix[0][i] = 1;
+        }
+        for(int i = 0 ;i<m;i++){
+            matrix[i][0] = 1;
+        }
+
+        for(int k =  1;k <m;k++)   { // to change columns
+            for(int j = 1;j<n;j++ ){   //to fill rows
+            matrix[k][j] = matrix[k - 1][j] + matrix[k][j-1];
+            }
+        }  
+
+        return matrix[m-1][n-1];
+    }
+};
